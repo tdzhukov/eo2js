58b621b74fa990cee6b1386464ae76904fb0d4e0
tdzhukov <tdzhukov@edu.hse.ru>
2023-05-01 00:34:21 +0300

    Remove artefacts

--- sandbox/compile.sh
+++ sandbox/compile.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 cd .. && mvn -DskipTests clean install
+npm install
 cd sandbox || exit
 mvn clean compile
 
 cp ../atoms/atoms.js ./target/generated-sources/
-cp ../atoms/package.json ./target/generated-sources/
 cd ./target/generated-sources/
 
 if [ "$1" = "--debug" ]; then
