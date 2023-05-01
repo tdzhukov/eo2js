e378dca7a41b7a0cecb2d8b26de978734776de68
tdzhukov <tdzhukov@edu.hse.ru>
2023-05-01 16:16:52 +0300

    Add CI: build and test

--- sandbox/test.sh
+++ sandbox/test.sh
@@ -1,3 +1,4 @@
+cd sandbox
 [ ! -d "./target" ] && echo "Directory target does not exists. Please, run compile.sh" 
 sed -i 's/^class /export class /g' ./target/generated-sources/*.js
-npm test
+mocha ./test/*.test.js
