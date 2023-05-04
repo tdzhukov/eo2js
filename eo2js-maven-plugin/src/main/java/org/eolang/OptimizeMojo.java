aec573a530d4a816fc5aba51bb854bef383131aa
nnv-nick <niknas02@mail.ru>
2023-05-04 22:08:00 +0300

    Some fixes

--- eo2js-maven-plugin/src/main/java/org/eolang/OptimizeMojo.java
+++ eo2js-maven-plugin/src/main/java/org/eolang/OptimizeMojo.java
@@ -108,7 +108,8 @@ public final class OptimizeMojo extends AbstractMojo {
                     "/org.eolang.maven/optimize/abstracts-float-up.xsl",
                 //     "/org.eolang.maven/optimize/remove-levels.xsl",
                 //     "/org/eolang/parser/add-refs.xsl",
-                    "/org.eolang.maven/optimize/fix-missed-names.xsl"
+                    "/org.eolang.maven/optimize/fix-missed-names.xsl",
+                    "/org.eolang.maven/optimize/other-to-abstracts.xsl"
                 //     "/org/eolang/parser/errors/broken-refs.xsl"
                 ).back()
             ).pass(new XMLDocument(file));
